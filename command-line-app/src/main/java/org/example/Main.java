a60fe310833b5227255193b94a794d166adee0f5
Can MOGOL <canmogol@gmail.com>
2023-12-27 23:30:59 +0300

    Move authorization into filter.

--- command-line-app/src/main/java/org/example/Main.java
+++ command-line-app/src/main/java/org/example/Main.java
@@ -24,12 +24,11 @@ public class Main {
                 System.exit(1);
             }
 
-
-            // Authenticate the user
-            String token = args[2];
             AuthenticationFilter authenticationFilter = new AuthenticationFilter();
-            authenticationFilter.authenticate(token);
+            authenticationFilter.authenticate(args);
 
+            AuthorizationFilter authorizationFilter = new AuthorizationFilter();
+            authorizationFilter.authorize(args);
 
             ProductController productController = new ProductController();
             if ("GET".equals(httpMethod)) {
@@ -37,16 +36,6 @@ public class Main {
                 System.out.println(products);
 
             } else if ("DELETE".equals(httpMethod)) {
-
-                // Authorization check
-                // admin users can delete products
-                // [_, admin, _]
-                String[] tokenParts = token.split("\\.");
-                String role = tokenParts[1];
-                // Authorization Filter
-                AuthorizationFilter authorizationFilter = new AuthorizationFilter();
-                authorizationFilter.authorize(role);
-
                 String productToDelete = path.substring("/products/".length());
                 List<String> products = productController.remove(productToDelete);
                 System.out.println(products);
@@ -61,10 +50,11 @@ public class Main {
 }
 
 class AuthenticationFilter {
-    public void authenticate(String token) {
-        if (token.equals("john.admin.rest-of-the-token")) {
-            System.out.println("authenticated, valid token");
-        } else if (token.equals("mike.user.rest-of-the-token")) {
+    public void authenticate(String[] args) {
+        // Authenticate the user
+        String token = args[2];
+        if (token.equals("john.admin.rest-of-the-token")
+                || token.equals("mike.user.rest-of-the-token")) {
             System.out.println("authenticated, valid token");
         } else {
             System.out.println("invalid token");
@@ -74,10 +64,17 @@ class AuthenticationFilter {
 }
 
 class AuthorizationFilter {
-    public void authorize(String role) {
-        if ("admin".equals(role)) {
-            System.out.println("authorized, admin user");
-        } else {
+    public void authorize(String[] args) {
+        String httpMethod = args[0];
+        String path = args[1];
+        String token = args[2];
+        // admin users can delete products
+        // [_, admin, _]
+        String[] tokenParts = token.split("\\.");
+        String role = tokenParts[1];
+        if ("DELETE".equals(httpMethod)
+                && path.startsWith("/products")
+                && !role.equals("admin")) {
             System.out.println("unauthorized, not an 'admin'");
             System.exit(1);
         }
